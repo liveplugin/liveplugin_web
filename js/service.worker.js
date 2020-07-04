5e744a155dba459123a47503b42f234d7d89b8a2
LivePlugin <liveplugin0@gmail.com>
2020-07-04 15:57:05 -0300

    sync

--- js/service.worker.js
+++ js/service.worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('flutter_service_worker.js');
+        navigator.serviceWorker.register('https://cdn.jsdelivr.net/gh/liveplugin/liveplugin_web@v1.0.4/flutter_service_worker.js');
     });
 }
